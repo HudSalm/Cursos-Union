272dd43e67ae796ca7102c6e57f520f8a4e8e1e4
Hudson <hudson.salm2001@outlook.com>
2025-10-27 15:38:20 -0300

    Adicionado a ligação do supabase para guardar os dados do formulario

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
+const SUPABASE_URL = 'https://pzlgalsmyrldtreifcuv.supabase.co';
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6bGdhbHNteXJsZHRyZWlmY3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1Nzg3NjcsImV4cCI6MjA3NzE1NDc2N30.KspBFfMckMtmyhGhRm67sk0SlNE3YVm8LK_nMMxKK4g';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+console.log('Script carregado. Cliente Supabase:', supabase);
+
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.getElementById('menu-toggle');
   const menuNav = document.getElementById('menu-nav');
   const selectEstado = document.getElementById('select-estado');
   const selectCidade = document.getElementById('select-cidade');
+
+  const form = document.getElementById('formulario');
+  const statusEl = document.getElementById('status');
+
+  if (!form) {
+    console.error('Erro: Formulário #formulario não encontrado');
+  }
+
+  form.addEventListener('submit', async event => {
+    event.preventDefault(); // impede a página de recarregar
+
+    const submitButton = form.querySelector('button');
+    submitButton.disabled = true;
+    statusEl.className = 'show loading';
+    statusEl.textContent = 'Enviando, por favor aguarde...';
+
+    const nomeVal = document.getElementById('nome').value;
+    const dddVal = document.getElementById('ddd').value;
+    const telefoneVal = document.getElementById('telefone').value;
+    const estadoVal = document.getElementById('select-estado').value;
+    const cidadeVal = document.getElementById('select-cidade').value;
+    const bairroVal = document.getElementById('bairro').value;
+    const emailVal = document.getElementById('email').value;
+
+    const { data, error } = await supabase.from('formulario_contato').insert([
+      {
+        nome: nomeVal,
+        ddd: dddVal,
+        numero: telefoneVal,
+        estado: estadoVal,
+        cidade: cidadeVal,
+        bairro: bairroVal,
+        email: emailVal,
+      },
+    ]);
+
+    if (error) {
+      console.error('Erro no Supabase', error.message);
+      statusEl.className = 'show error';
+      statusEl.textContent = 'Erro ao enviar' + error.message;
+      submitButton.disabled = false;
+    } else {
+      console.log('Dados salvos', data);
+      statusEl.className = 'show success';
+      statusEl.textContent = 'Dados enviados com sucesso!';
+
+      form.reset();
+
+      setTimeout(() => {
+        submitButton.disabled = false;
+        statusEl.textContent = '';
+      }, 5000);
+    }
+  });
 
   async function carregarEstados() {
     const response = await fetch(
